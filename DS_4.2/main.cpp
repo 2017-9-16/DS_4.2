fc25204c8edaf25be8289fb2de5d045b95a8446d
陈翰 <chenhan199781@gmail.com>
2018-05-13 10:26:57 +0800

    Initial Commit

---
+++ DS_4.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS_4.2
+//
+//  Created by Walter Mitty on 2018/5/13.
+//  Copyright © 2018年 Walter Mitty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
